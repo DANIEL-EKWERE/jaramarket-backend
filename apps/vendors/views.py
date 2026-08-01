37d684587bae8cb711622a44655a34594a90fad2
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2026-08-01 18:50:08 +0100

    added1

--- apps/vendors/views.py
+++ apps/vendors/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
 from api.utils import success
 from .models import Market
@@ -6,6 +7,7 @@ from .serializers import MarketSerializer
 
 
 @api_view(["GET"])
+@permission_classes([AllowAny])
 def markets_collection(request):
     markets = Market.objects.filter(is_active=True).order_by("name")
     return success("Markets retrieved successfully", MarketSerializer(markets, many=True).data)
